dc4f29bad2ac045b29b07b0953f56c7b5cccfef9
AndrewWang12345 <144137738+AndrewWang12345@users.noreply.github.com>
2024-05-14 03:41:04 -0400

    Create My Coin.sol

---
+++ My Coin.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.0;
+
+contract MyCoin {
+    string public constant name = "My Coin";
+    string public constant symbol = "\xF0\x9F\x93\xB7";
+    uint8 public constant decimals = 9;
+    uint256 public constant totalSupply = 1000000000 * (10 ** uint256(decimals)); // Total supply of 1,000,000,000 tokens
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply; // Assign total supply to the contract creator
+    }
+
+   function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_from != address(0), "Invalid address");
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
